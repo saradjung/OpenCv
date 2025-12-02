35c1c14919fc85131b8ebdd10a6b3912fae41399
Sarad <thapasarad87@gmail.com>
2025-12-02 22:51:01 +0545

    resize/rescale

--- index.py
+++ index.py
@@ -1,19 +1,33 @@
 import cv2 as cv
 # reading images
-# image=cv.imread('Static/ghost.png')
+image=cv.imread('Static/ghost.png')
 
-# cv.imshow('Ghost', image)
+cv.imshow('Ghost', image)
 
 # cv.waitKey(0)
-captureVid=cv.VideoCapture('Static/test.mp4')
 
-while True:
-    isTrue, frame= captureVid.read()
+def resize_fun(frame, scale=0.75):
+    height=int(frame.shape[0] *scale)
+    width= int(frame.shape[1]* scale)
 
-    cv.imshow('Video',frame)
+    dimension=(width, height)
 
-    if cv.waitKey(20) & 0xFF== ord('d'):
-        break
+    return cv.resize(frame, dimension, interpolation=cv.INTER_AREA)
 
-captureVid.release()
-cv.destroyAllWindows()
+# captureVid=cv.VideoCapture('Static/test.mp4')
+
+# while True:
+#     isTrue, frame= captureVid.read()
+
+#     frame_resized=resize_fun(frame)
+#     cv.imshow('Video',frame)
+#     cv.imshow('vid1',frame_resized)
+#     if cv.waitKey(20) & 0xFF== ord('d'):
+#         break
+#captureVid.release()
+# cv.destroyAllWindows()
+
+im_resized=resize_fun(image)
+cv.imshow('im1',im_resized)
+cv.waitKey(0)
+# 
