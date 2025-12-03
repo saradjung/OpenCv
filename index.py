197e7f4e6f62d4bcda82d09fc0f9946a5f797b4b
Sarad <thapasarad87@gmail.com>
2025-12-03 20:58:59 +0545

    functions basic 5

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 # reading images
-# image=cv.imread('Static/ghost.png')
+image=cv.imread('Static/ghost.png')
 
-# cv.imshow('Ghost', image)
+cv.imshow('Ghost', image)
 
 # # cv.waitKey(0)
 
@@ -32,3 +32,27 @@ import cv2 as cv
 # cv.waitKey(0)
 # # 
 
+# gray=cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+
+# cv.imshow('colorchange',gray)
+
+# blur=cv.GaussianBlur(image,(3,3),cv.BORDER_DEFAULT)
+# cv.imshow('blur',blur)
+
+#edge cascade
+# edge=cv.Canny(image,(100),(200))
+# cv.imshow('Edge',edge)
+
+# dilated=cv.dilate(edge,(3,3),iterations=3)
+# cv.imshow('Dilated',dilated)
+
+# eroded=cv.erode(dilated,(3,3),iterations=3)
+# cv.imshow('eroded',eroded)
+
+cropped=image[20:250,40:300]
+cv.imshow("Cropeed",cropped)
+
+
+
+
+cv.waitKey(0)
